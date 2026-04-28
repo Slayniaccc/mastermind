a16d0e48573553ba07e82e4a81debe02a8b1a45c
Slayniaccc <212771087+Slayniaccc@users.noreply.github.com.>
2026-04-28 19:53:55 +0100

    Implement core gameplay loop and secret code generation

--- mastermind.java
+++ mastermind.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 public class mastermind {
+
+    private static Random random = new Random();
 
     public static void main(String[] args) {
         runGame();
@@ -9,7 +13,7 @@ public class mastermind {
 
         while (running) {
             int mode = selectMode();
-            playRound(mode); // NEW LINE ADDED
+            playRound(mode);
             running = false;
         }
     }
@@ -19,13 +23,51 @@ public class mastermind {
         return 1;
     }
 
-    // NEW METHOD
     private static void playRound(int mode) {
         int codeLength = 4;
         int attempts = 6;
 
-        System.out.println("Round started");
-        System.out.println("Code length: " + codeLength);
-        System.out.println("Attempts: " + attempts);
+        char[] secret = generateCode(codeLength);
+
+        int tries = 0;
+        boolean won = false;
+
+        while (tries < attempts && !won) {
+            System.out.println("Attempt " + (tries + 1));
+
+            char[] guess = captureGuess(codeLength);
+
+            if (Arrays.equals(secret, guess)) {
+                won = true;
+                break;
+            }
+
+            tries++;
+        }
+
+        if (won) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("You lose! Code was: " + new String(secret));
+        }
+    }
+
+    private static char[] generateCode(int length) {
+        char[] colours = {'R','G','B','Y','O','P'};
+        List<Character> pool = new ArrayList<>();
+
+        for (char c : colours) pool.add(c);
+        Collections.shuffle(pool);
+
+        char[] code = new char[length];
+        for (int i = 0; i < length; i++) {
+            code[i] = pool.get(i);
+        }
+
+        return code;
+    }
+
+    private static char[] captureGuess(int length) {
+        return new char[length];
     }
 }
